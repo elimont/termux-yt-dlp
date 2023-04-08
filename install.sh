dd591ab98f6b8e83c3f27b92ce27f6c6927ac07c
elimont <30360365+elimont@users.noreply.github.com>
2023-04-08 00:51:19 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/data/data/com.termux/files/usr/bin/bash
 
 BLUE='\e[34m'
 NC='\e[0m'
@@ -9,8 +8,8 @@ TERMUXURLOPENER_CONFIG_FOLDER="${HOME}/bin/"
 echo "Hi, This script setup an environment to download various videos from various apps"
 sleep 1
 echo -e "\n\n${BLUE}Requirements:${NC}"
-echo -e "    1. Allow storage access to Termux."
-echo -e "    2. A working internet connection.\n\n"
+echo "    1. Allow storage access to Termux."
+echo "    2. A working internet connection."
 read -p "When you are ready just press enter:"
 
 # Basic setup
@@ -22,7 +21,7 @@ pip install -U pip
 pip install -U wheel
 pip install -U yt-dlp
 mkdir -p $TERMUXURLOPENER_CONFIG_FOLDER
-cp -r -u yt-dlp ~/.config/
+cp -r -u $YTDLP_CONFIG_FOLDER ~/.config/
 cp termux-url-opener "${TERMUXURLOPENER_CONFIG_FOLDER}/"
 
 echo -e "${BLUE}Congratulations!!! Your setup is complete.${NC}\n\n"
